bd729109543f61f6a41bc7277d149e00018fc5ef
astritvyoo-ui <astritvyoo@gmail.com>
2026-09-14 16:14:55 +0700

    Menambahkan latihan Java

---
+++ src/Variabel.java
@@ -0,0 +1,11 @@
+public class Variabel {
+    public static void main(String[] args) {
+
+        int umur = 20;
+        double gaji = 3500000.50;
+        boolean lulus = true;
+        String nama = "Budi";
+        
+        System.out.println("Nama saya adalah: " + nama);
+    }
+}
